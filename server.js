b2f4d216c3c32ac3a513e5a4ec381eb223892d6a
krupavarma <krupavarma1995@gmail.com>
2018-06-04 16:48:22 +0530

    ppppppp

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ router.use(express.static('dist'));
 process.env.NODE_PATH = __dirname;
 var server_port =  5001
 var server_ip_address = '0.0.0.0';
-server.listen( server_port,server_ip_address, function(){
+server.listen( server_port, function(){
  var addr = server.address();
   console.info('APP server listening at', addr.address + ': '+ addr.port);
 });
